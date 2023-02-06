65c4df2012da00f8f3b311fbacae64f99523edfd
Dennis Rieks <dennisrieks@googlemail.com>
2023-02-06 22:34:16 +0100

    Breaking: publish to maven central using github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,16 @@ buildscript {
     repositories {
         mavenLocal()
         mavenCentral()
-        jcenter()
-        maven("https://oss.jfrog.org/oss-snapshot-local/")
     }
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
+}
+
+plugins {
+    `maven-publish`
+    java
 }
 
 // a small hack: the variable must be named like the property
@@ -28,6 +31,9 @@ val groupProperty = if (group.endsWith(".antlr-kotlin")) {
 }
 
 allprojects {
+    apply(plugin = "maven-publish")
+    apply(plugin = "java")
+
     // ... because `version` is another var here.
     // when version is hardcoded here, jitpack can not overwrite it.
     // the default version can now be changed in gradle.properties
@@ -41,11 +47,62 @@ allprojects {
         options.isDeprecation = true
     }
 
+    java {
+        withSourcesJar()
+        withJavadocJar()
+    }
+
     repositories {
         mavenLocal()
         mavenCentral()
-        jcenter()
-        maven("https://oss.jfrog.org/oss-snapshot-local/")
+    }
+
+    publishing {
+        publications {
+            publications.configureEach {
+                if (this is MavenPublication) {
+                    pom {
+                        name.set("antlr-kotlin")
+                        description.set("Support for Kotlin as a target for ANTLR")
+                        url.set("http://www.example.com/library")
+                        licenses {
+                            license {
+                                name.set("The Apache License, Version 2.0")
+                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("ftomassetti")
+                                name.set("Federico Tomassetti")
+                                email.set("federico@strumenta.com")
+                            }
+                            developer {
+                                id.set("drieks")
+                                name.set("Dennis Rieks")
+                                email.set("dennisrieks@googlemail.com")
+                            }
+                        }
+                        scm {
+                            connection.set("scm:git:git@github.com:Strumenta/antlr-kotlin.git")
+                            developerConnection.set("scm:git:git@github.com:Strumenta/antlr-kotlin.git")
+                            url.set("https://github.com/Strumenta/antlr-kotlin")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "OSSRH"
+                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
     }
 }
 
